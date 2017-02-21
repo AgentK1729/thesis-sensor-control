466d8cf6f355fabf4c48de874359484cec1c3b15
Tejas Sathe <sathe.tejas@gmail.com>
2017-02-21 12:11:30 -0500

    Added volume sniffing

--- sniffer.py
+++ sniffer.py
@@ -1,9 +1,37 @@
 import NetworkManager as nmg
+from sqlite3 import connect
+from time import sleep
 
-for dev in nmg.NetworkManager.GetDevices():
-    if dev.DeviceType != nmg.NM_DEVICE_TYPE_WIFI:
-        continue
+def sniff():
+	while True:
+		ssids = {}
 
-    aps = dev.SpecificDevice().GetAccessPoints()
-    for ap in sorted(aps, key=lambda ap: ap.Strength):
-        print ap.Ssid, ap.Strength
+		for dev in nmg.NetworkManager.GetDevices():
+		    if dev.DeviceType != nmg.NM_DEVICE_TYPE_WIFI:
+		        continue
+
+		    # key[0] = count, key[1] = total strength
+		    aps = dev.SpecificDevice().GetAccessPoints()
+		    for ap in sorted(aps, key=lambda ap: ap.Strength):
+		        ssids[ap.Ssid] = ssids.get(ap.Ssid, [0, 0])
+		        ssids[ap.Ssid][0] += 1
+		        ssids[ap.Ssid][1] += ap.Strength
+		        if ap.Ssid == "UMBC Campus":
+		        	print ap.Ssid, ap.Frequency
+
+
+		ssids_list = [(key, item[0], item[1]) for key, item in ssids.iteritems()]
+
+		conn = connect("ssid.sqlite")
+		cur = conn.cursor()
+		query = "insert into signals values (?, ?, ?)"
+
+		for tup in ssids_list:
+			cur.execute(query, tup)
+
+		conn.commit()
+		conn.close()
+
+		sleep(30)
+
+sniff()
--- temp.py
+++ temp.py
@@ -3,12 +3,12 @@ import sys,os
 import socket
 import binascii
 import pcapy
-from time import sleep
+from time import time, sleep
 
 def sniff():
 	rawSocket = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x0800))
 	#ifconfig eth0 promisc up
-	receivedPacket = rawSocket.recv(2048)
+	receivedPacket = rawSocket.recv(65565)
 
 	#TCP Header...
 	tcpHeader = receivedPacket[34:54]
@@ -17,20 +17,22 @@ def sniff():
 		sourcePort = socket.inet_ntoa(tcpHdr[0])
 		destinationPort = socket.inet_ntoa(tcpHdr[1])
 		
-		return (sourcePort, destinationPort)
+		# return (sourcePort, destinationPort)
+		return True
 
 	except Exception as e:
-		print "Exception"
-		return ("exception-source", "exception-destination")
+		# return ("exception-source", "exception-destination")
+		return False
 
 
-volume = {}
-count = 0
-while count < 10:
-	print count
-	(source, dest) = sniff()
-	volume[source] = volume.get(source, -1) - 1
-	volume[dest] = volume.get(dest, 1) + 1
-	count += 1
+while True:
+	end = time() + 10
+	packets = 0
+	print "Gathering",
+	while time() < end:
+		if sniff():
+			packets += 1
 
-print volume
+	print packets
+
+	sleep(10)
